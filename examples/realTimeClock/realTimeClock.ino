d108ee8872df3ddc7d87e42935f6cccd3444a1fe
davide <davidebotti92@gmail.com>
2023-06-30 09:55:43 +0200

    Example for rtc completed

--- examples/realTimeClock/realTimeClock.ino
+++ examples/realTimeClock/realTimeClock.ino
@@ -6,24 +6,36 @@ RTC_DS1307 rtc;
 void setup() {
   Serial.begin(9600);
 
+  if(!rtc.begin()) {
+    Serial.println("Error!");
+    return;
+  }
+  
   if(!rtc.isrunning()) {
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 }
 
+String days[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
 void loop() {
   DateTime now = rtc.now();
+
   Serial.print(now.year(), DEC);
   Serial.print('/');
   Serial.print(now.month(), DEC);
   Serial.print('/');
   Serial.print(now.day(), DEC);
-  Serial.print(' (');
-  Serial.print(now.dayOfTheWeek(), DEC);
-  Serial.print(') ');
+  Serial.print(" ");
+  Serial.print(days[now.dayOfTheWeek()-1]);
+  Serial.print(" ");
   Serial.print(now.hour(), DEC);
   Serial.print(':');
   Serial.print(now.minute(), DEC);
   Serial.print(':');
   Serial.print(now.second(), DEC);
+
+  Serial.println();
+
+  delay(10000);
 }
