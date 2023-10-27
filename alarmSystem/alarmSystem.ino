ab276bfbb23d3d4815130a2ada0c894637574f60
Davide Botti <davidebotti92@gmail.com>
2023-10-27 12:18:12 +0200

    Fast commit

--- alarmSystem/alarmSystem.ino
+++ alarmSystem/alarmSystem.ino
@@ -1,9 +1,65 @@
-void setup() {
-  // put your setup code here, to run once:
+/**
+  ABOUT PIR SENSOR
+    Position is: knobs up (all'insu) and yellow selector on the left
+    selector in single selector mode, remains high only for the time of the delay, low for 3 seconds, then repeat
+    selector in repetable trigger, remains high as long as it detects movement and the delay is restarted every new detection
+    left knob is range adjustment: clockwise turn, increase range/sensibility
+    right knob is the time delay adjustment: clockwise turn, increase timing
 
+    Wiring:
+    middle pin is signal (to pin 2)
+    left pin ground
+    right pin Vcc
+
+    USAGE MODE: single mode with low delay. With this settings, delay is 9s
+**/
+
+#define ALARM_SENSOR 2
+#define ALARM 13
+#define ALARM_DURATION 1000
+
+uint8_t alarm;
+long timestamp, lastAlarm;
+
+
+void setup() {
+  // Serial.begin(9600);
+  pinMode(ALARM_SENSOR, INPUT);
+  pinMode(ALARM, OUTPUT);
+  delay(1000);  //For PIR sensor to set up
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  timestamp = millis();
+  alarm = digitalRead(ALARM_SENSOR);
+  // Serial.print("Alarm status:");
+  // Serial.println(alarm);
+  if(alarm == HIGH) {
+    lastAlarm = timestamp;
+    tone(ALARM, 440 ,ALARM_DURATION);
+  } else {
+    noTone(ALARM);
+  }
 }
+
+
+/**
+EXAMPLE SKETCH FOR TESTING
+#define ALARM_SENSOR 2
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ALARM_SENSOR, INPUT);
+}
+uint8_t alarm;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  alarm = digitalRead(ALARM_SENSOR);
+  Serial.print("Alarm status:");
+  Serial.println(alarm);
+  delay(1000);
+}
+**/
