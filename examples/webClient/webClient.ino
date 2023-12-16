2d500c549fb845ff978965bc10e3a2092d4081f6
Davide Botti <davidebotti92@gmail.com>
2023-12-16 10:47:23 +0100

    Fast commit

--- examples/webClient/webClient.ino
+++ examples/webClient/webClient.ino
@@ -1,45 +1,40 @@
-/*
-  Repeating WiFi Web Client
-
- This sketch connects to a a web server and makes a request
- using a WiFi equipped Arduino board.
-
- created 23 April 2012
- modified 31 May 2012
- by Tom Igoe
- modified 13 Jan 2014
- by Federico Vanzati
-
- This code is in the public domain.
-
-  Find the full UNO R4 WiFi Network documentation here:
-  https://docs.arduino.cc/tutorials/uno-r4-wifi/wifi-examples#wi-fi-web-client-repeating
- */
-
 #include "WiFiS3.h"
+#include <ArduinoJson.h>
 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = "FRITZ!Box 7530 QR";        // your network SSID (name)
 char pass[] = "06989642165939857128";    // your network password (use for WPA, or use as key for WEP)
 
 int status = WL_IDLE_STATUS;
 
-// Initialize the WiFi client library
 WiFiClient client;
 
 // server address:
 char server[] = "davides-mbp";
 // IPAddress server(192, 168, 178, 21);
 
-unsigned long lastConnectionTime = 0;            // last time you connected to the server, in milliseconds
-const unsigned long postingInterval = 10L * 1000L; // delay between updates, in milliseconds
+unsigned long lastConnectionTime = 0;
+const unsigned long postingInterval = 10L * 1000L;
 
+void httpRequest();
 void performGET();
 void performPOST(int param);
+// IMPORTANT: these read method are the same, only thing different are the json fields to be retrieved
+void read_GET_response();
+void read_POST_response();
 
-/* -------------------------------------------------------------------------- */
+  // Allocate the JSON document
+  //
+  // Inside the brackets, 200 is the capacity of the memory pool in bytes.
+  // Don't forget to change this value to match your JSON document.
+  // Use arduinojson.org/v6/assistant to compute the capacity.
+  StaticJsonDocument<100> doc;
+
+  // StaticJsonDocument<N> allocates memory on the stack, it can be
+  // replaced by DynamicJsonDocument which allocates in the heap.
+  //
+  // DynamicJsonDocument doc();
+
 void setup() {
-/* -------------------------------------------------------------------------- */  
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
@@ -73,45 +68,89 @@ void setup() {
   printWifiStatus();
 }
 
-/* just wrap the received data up to 80 columns in the serial print*/
-/* -------------------------------------------------------------------------- */
-void read_request() {
-/* -------------------------------------------------------------------------- */  
-  uint32_t received_data_num = 0;
-
-  while (client.available()) {
-    /* actual data reception */
-    char c = client.read();
-    /* print data to serial port */
-    Serial.print(c);
-    /* wrap data to 80 columns*/
-    received_data_num++;
-    if(received_data_num % 80 == 0) { 
-      
-    } 
-  }
-}
-
-/* -------------------------------------------------------------------------- */
 void loop() {
-/* -------------------------------------------------------------------------- */  
   // if there's incoming data from the net connection.
   // send it out the serial port.  This is for debugging
   // purposes only:
-  read_request();
+
+  // read_GET_response();
+  read_POST_response();
   
-  // if ten seconds have passed since your last connection,
-  // then connect again and send data:
   if (millis() - lastConnectionTime > postingInterval) {
     httpRequest();
   }
 
 }
 
+void read_GET_response() {
+  String json = "";
+
+  while (client.available()) {
+    /* actual data reception */
+    char c = client.read();
+    if(c == '\n') {
+      c = client.read();
+      if(c == '\r') {
+        c = client.read();
+        json += c;
+        while(c != '}') {
+          c = client.read();
+          json += c;
+        }
+      }
+    }
+  }
+
+  if(json.length() > 0) {
+    Serial.println("JSON response was: " + json);
+    DeserializationError error = deserializeJson(doc, json);
+
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    Serial.println("User was: " + doc["name"].as<String>());
+    Serial.println("Age was: " + String(doc["age"].as<int>()));
+  }
+}
+
+void read_POST_response() {
+  String json = "";
+
+  while (client.available()) {
+    /* actual data reception */
+    char c = client.read();
+    if(c == '\n') {
+      c = client.read();
+      if(c == '\r') {
+        c = client.read();
+        json += c;
+        while(c != '}') {
+          c = client.read();
+          json += c;
+        }
+      }
+    }
+  }
+
+  if(json.length() > 0) {
+    Serial.println("JSON response was: " + json);
+    DeserializationError error = deserializeJson(doc, json);
+
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    Serial.println("Number was: " + doc["yourNumber"].as<String>());
+  }
+}
+
 // this method makes a HTTP connection to the server:
-/* -------------------------------------------------------------------------- */
 void httpRequest() {
-/* -------------------------------------------------------------------------- */  
   // close any connection before send a new request.
   // This will free the socket on the NINA module
   client.stop();
@@ -122,30 +161,28 @@ void httpRequest() {
 }
 
 void performGET() {
-    // if there's a successful connection:
   if (client.connect(server, 8080)) {
     Serial.println("connecting...");
-    // send the HTTP GET request:
+    
     client.println("GET /findUser?name=Francesca HTTP/1.1");
     client.println("Host: example.org");
     client.println("User-Agent: ArduinoWiFi/1.1");
     client.println("Connection: close");
     client.println();
-    // note the time that the connection was made:
+    
     lastConnectionTime = millis();
   } else {
-    // if you couldn't make a connection:
     Serial.println("connection failed");
   }
+
+  
 }
 
 void performPOST(int param) {
-    String requestBody = "{\"number\":" + String(param) + "}";
+  String requestBody = "{\"number\":" + String(param) + "}";
 
-  // if there's a successful connection:
   if (client.connect(server, 8080)) {
     Serial.println("connecting...");
-    // send the HTTP POST request:
     client.println("POST /arduino/test HTTP/1.1");
     client.println("Content-Type: application/json;charset=UTF-8");
     client.println("User-Agent: ArduinoWiFi/1.1");
@@ -154,18 +191,13 @@ void performPOST(int param) {
     client.println(requestBody.length());
     client.println();
     client.println(requestBody);
-    // note the time that the connection was made:
     lastConnectionTime = millis();
   } else {
-    // if you couldn't make a connection:
     Serial.println("connection failed");
   }
 }
 
-/* -------------------------------------------------------------------------- */
 void printWifiStatus() {
-/* -------------------------------------------------------------------------- */  
-  // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
